5a313fc0bbb7107509d3f2e09deeb5a36ea936dc
rzzz0 <rudyhsurh@gmail.com>
2023-09-11 13:32:58 +0800

    updated solution

--- src/main/java/com/example/VariablesAssignment.java
+++ src/main/java/com/example/VariablesAssignment.java
@@ -9,38 +9,45 @@ public class VariablesAssignment {
     short shortVariable=32765;
 
     // TODO: Declare and initialize an int variable with any positive value
-    int intVariable;
+    int intVariable=2;
 
     // TODO: Declare and initialize a long variable with any positive value
-    long longVariable;
+    long longVariable=29999;
 
     // TODO: Declare and initialize a float variable with any positive value
-    float floatVariable;
+    float floatVariable=30.3f;
 
     // TODO: Declare and initialize a double variable with any positive value
-    double doubleVariable;
+    double doubleVariable=89.8;
 
     // TODO: Declare and initialize a char variable with the initial of your first name
-    char charVariable;
+    char charVariable='R';
 
     // TODO: Declare and initialize a boolean variable with the value true or false
     boolean booleanVariable=true;
 
     // TODO: Create a method that accepts two integers as parameters and returns their sum
     public int sum(int a, int b) {
+        
         // Your code here
-        return 0; // Change this line to return the actual sum
+        int ans = a + b;
+        return ans; // Change this line to return the actual sum
     }
 
     // TODO: Create a method that accepts two double parameters and returns their product
     public double product(double a, double b) {
+        double ans = a*b;
         // Your code here
-        return 0; // Change this line to return the actual product
+        return ans; // Change this line to return the actual product
     }
 
     // TODO: Create a method that accepts a string parameter and returns it in reverse
     public String reverseString(String str) {
+        StringBuilder reversed = new StringBuilder();
+        for (int i = str.length() -1; i >= 0; i --){
+            reversed.append(str.charAt(i));
+        }
         // Your code here
-        return ""; // Change this line to return the reversed string
+        return reversed.toString(); // Change this line to return the reversed string
     }
 }
